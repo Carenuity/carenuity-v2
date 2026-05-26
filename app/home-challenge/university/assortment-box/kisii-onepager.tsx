53eebee09a0a115a47e1cdc2aa135ec37ad08897
mevica <mevicandanu@gmail.com>
2026-05-26 13:50:47 +0300

    feat: developing products, more on sq-panel,home challenge

--- app/home-challenge/university/assortment-box/kisii-onepager.tsx
+++
@@ -1,8 +0,0 @@
-export default function KisiiOnepager() {
-  return (
-    <iframe
-      src="/docs/kisii-onepager.pdf"
-      style={{ width: "100%", height: "100vh", border: "none" }}
-    />
-  );
-}
